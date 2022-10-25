3d9c2629c2b9bf374ef8e911a802fa8d00dcd060
azizlimonu <106480374+azizlimonu@users.noreply.github.com>
2022-10-25 23:27:27 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { store } from './store';
 import { Provider } from 'react-redux';
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
